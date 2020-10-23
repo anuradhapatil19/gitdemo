01b28224eb0c5f11fc775bcb6d45eb23daa7b526
Jaypal325 <70268337+Jaypal325@users.noreply.github.com>
2020-10-23 23:25:59 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,1 +1,20 @@
-print("first push to git")
+
+import smtplib
+
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+s.starttls()
+
+
+s.login("sender mail", "sender password")
+
+
+message = "otp testing"
+
+
+s.sendmail("sender_mail", "receiver_mail", message)
+
+s.quit()
+
+print("email sent...!")
